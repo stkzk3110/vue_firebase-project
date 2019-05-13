7a75170489575a4df5fd0baa17bf3d2e6fc2808d
fisherman0825 <fisherman0825dd@gmail.com>
2019-05-13 11:13:16 +0900

    Create Header and Footer and SNS link

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/skill">Skill</router-link> |
-      <router-link to="/contact">Contact</router-link>
+    <div class="header-outer">
+      <header>
+        <h1 class="site-title">
+          <router-link to="/">cube's portfolio</router-link>
+        </h1>
+      </header>
+      <nav id="gnav" class="header-nav">
+        <ul>
+          <li><router-link to="/">HOME</router-link></li>
+          <li><router-link to="/about">ABOUT</router-link></li>
+          <li><router-link to="/skill">SKILL</router-link></li>
+          <li><router-link to="/contact">CONTACT</router-link></li>
+        </ul>
+      </nav>
     </div>
     <router-view/>
+    <nav class="footer-nav">
+      <ul>
+        <li><router-link to="/">HOME</router-link></li>
+        <li><router-link to="/about">ABOUT</router-link></li>
+        <li><router-link to="/skill">SKILL</router-link></li>
+        <li><router-link to="/contact">CONTACT</router-link></li>
+      </ul>
+    </nav>
+    <div class="sns-box">
+      <a href="https://twitter.com/fisherman3110se" target="_blank" class="sns-link">
+          <i class="fab twitter-icon"></i>
+      </a>
+      <a href="https://github.com/stkzk3110" target="_blank" class="sns-link">
+          <i class="fab github-icon"></i>
+      </a>
+      <a href="https://instagram.com/stkzk_aioaui" target="_blank" class="sns-link">
+          <i class="fab instagram-icon"></i>
+      </a>
+      <a href="https://qiita.com/fisherman3110se" target="_blank" class="sns-link">
+          <i class="fab qiita-icon"></i>
+      </a>
+      <a href="https://note.mu/fisherman0825" target="_blank" class="sns-link">
+          <i class="fab note-icon"></i>
+      </a>
+    </div>
+    <footer>
+      <div class="copyright-box">©️ 2019 cube</div>
+    </footer>
   </div>
 </template>
 
